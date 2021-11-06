73e030740d25877f942652c81332aac7d5da605f
Anh Nguyen <93259247+NguyenAnh55@users.noreply.github.com>
2021-11-06 03:49:24 +0000

    Digit Type

--- ex6.c
+++ ex6.c
@@ -5,14 +5,30 @@ int main(){
         printf("Enter an integer between 1 and 15: ");
         scanf("%d ",&num);
     } while ( num < 1 || num >15 );
+    
     for (int row = 1; row <= num; row++){
+        
         for (int block = 1; block <= num - row; block++)
+        
         {
-            printf("   ");
+            
+            printf("    ");
+        
         }
+
         for (int block = row; block >=1; block--)
+        
         {
-            printf(" %d ", block);
-        }printf("\n");
+            
+            printf(" %2d ", block);
+        
+        }
+        for (int block = 2; block <=row;block++)
+        {
+
+            printf(" %2d ", block);
+        
+        }
+        printf("\n");
     }
 } 
